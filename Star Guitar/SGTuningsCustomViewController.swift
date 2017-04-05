26d9ca64aa529a8f37e4b0567ef8df0fedbe2b80
trujunzhang <trujunzhang@gmail.com>
2017-04-05 14:38:37 +0800

    Tuning notes.

--- Star Guitar/SGTuningsCustomViewController.swift
+++ Star Guitar/SGTuningsCustomViewController.swift
@@ -15,8 +15,14 @@ class SGTuningsCustomViewController: UIViewController {
     @IBOutlet weak var oneColumnContainer: UIView!
     @IBOutlet weak var twoColumnContainer: UIView!
     @IBOutlet weak var threeColumnContainer: UIView!
+    @IBOutlet weak var fretboardContainer: UIView!
     @IBOutlet weak var eventsContainer: UIView!
-    @IBOutlet weak var tuningsResultContainer: UIView!
+    
+    var oneColumnViewController: SGOneColumnViewController!
+    var twoColumnViewController: SGTwoColumnViewController!
+    var threeColumnViewController: SGThreeColumnViewController!
+    var fretboardViewController: SGFretboardViewController!
+    var tuningsEventViewController: SGTuningsEventViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +36,7 @@ class SGTuningsCustomViewController: UIViewController {
         TwoColumnLayout(self.view).layoutContainer(oneColumnContainer, forColumnView: twoColumnContainer)
         ThreeColumnLayout(self.view).layoutContainer(twoColumnContainer, forThirdColumnView: threeColumnContainer,0,ThreeColumnSchema.threeColumnWidth())
         
-        FretboardLayout(self.view).layoutFretboard(threeColumnContainer, forFretboardView: tuningsResultContainer,0,TuningTypeSchema.itemWidth() + FretboardColumnSchema.resultsMarginLeft() + FretboardBorderSchema.outBoardSize() + FretboardBorderSchema.marginLeft())
+        FretboardLayout(self.view).layoutFretboard(threeColumnContainer, forFretboardView: fretboardContainer,0,TuningTypeSchema.itemWidth() + FretboardColumnSchema.resultsMarginLeft() + FretboardBorderSchema.outBoardSize() + FretboardBorderSchema.marginLeft())
         
         TuningsEventsLayout(self.view).layoutContainer(self.view, forView: eventsContainer)
 
@@ -44,6 +50,20 @@ class SGTuningsCustomViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "titleContainer") {
             self.titleViewController = segue.destination as! TitleViewController
+        } else if (segue.identifier == "oneColumnContainer") {
+            self.oneColumnViewController = segue.destination as! SGOneColumnViewController
+        }
+        else if (segue.identifier == "twoColumnContainer") {
+            self.twoColumnViewController = segue.destination as! SGTwoColumnViewController
+        }
+        else if (segue.identifier == "threeColumnContainer") {
+            self.threeColumnViewController = segue.destination as! SGThreeColumnViewController
+        }
+        else if (segue.identifier == "fretboardContainer") {
+            self.fretboardViewController = segue.destination as! SGFretboardViewController
+        }
+        else if (segue.identifier == "eventsContainer") {
+            self.tuningsEventViewController = segue.destination as! SGTuningsEventViewController
         }
     }
 
