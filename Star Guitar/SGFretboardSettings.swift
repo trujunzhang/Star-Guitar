46f2a86bdced4530f2ddb1031b52f8601c81f250
trujunzhang <trujunzhang@gmail.com>
2017-04-07 21:29:15 +0800

    Tuning notes.

--- Star Guitar/SGFretboardSettings.swift
+++ Star Guitar/SGFretboardSettings.swift
@@ -1,5 +1,5 @@
 //
-//  SGTuningsCustomSettings.swift
+//  SGFretboardSettings.swift
 //  Star Guitar
 //
 //  Created by djzhang on 4/7/17.
@@ -12,27 +12,17 @@ import RealmSwift
 
 
 
-//
-// ===
-// Realm Object
-//
-class TuningsCustomSettings: Object {
-    
-    dynamic var id = 0
-    
-    override static func primaryKey() -> String? {
-        return "id"
-    }
-    
-    // 6X5
-    dynamic var results: String = TuningsCustomSettingsModel.getDefaultResults()
-    
-}
-
-
-class TuningsCustomSettingsModel {
-    public static func getDefaultResults() -> String {
-        return String(format: "%@;%@;%@;%@;%@",
+class TuningsFretboardSettingsModel {
+    // 6X12
+    public static func getDefaultFretboard() -> String {
+        return String(format: "%@;%@;%@;%@;%@;%@;%@;%@;%@;%@;%@;%@",
+                      "-1,-1,-1,-1,-1,-1",
+                      "-1,-1,-1,-1,-1,-1",
+                      "-1,-1,-1,-1,-1,-1",
+                      "-1,-1,-1,-1,-1,-1",
+                      "-1,-1,-1,-1,-1,-1",
+                      "-1,-1,-1,-1,-1,-1",
+                      "-1,-1,-1,-1,-1,-1",
                       "-1,-1,-1,-1,-1,-1",
                       "-1,-1,-1,-1,-1,-1",
                       "-1,-1,-1,-1,-1,-1",
@@ -48,11 +38,11 @@ class TuningsCustomSettingsModel {
         // Empty here.
         //self.resultCells = TuningsFactory.convert(TuningsStandardSettingsModel.getDefaultResults())
         
-        self.resultCells = TuningsStandardFactory.getStandardResultsCells()
+        //self.resultCells = TuningsFretboardSettingsModel.getStandardResultsCells()
     }
     
     init( _ results: String) {
-        self.resultCells = TuningsStandardFactory.convert(results)
+        //self.resultCells = TuningsFretboardSettingsModel.convert(results)
     }
     
     public func getResults() -> String{
@@ -68,17 +58,4 @@ class TuningsCustomSettingsModel {
         //settings.results = self.results
         return settings
     }
-}
-
-
-
-class TuningsCustomSettingsUtils: TuningsBaseSettingsUtils {
-
-    override init() {
-        super.init()
-        //self.readSettings()
-    }
-    
-    
-    
 }
